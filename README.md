3f899804cf4166eaa8149dd71aff5f4974d5c4d7
Marvin Luchs <marvin.luchs@holisticon.de>
2019-05-07 21:35:57 +0200

    reset repository

--- README.md
+++
@@ -1,1 +0,0 @@
-# hypercontract-shared
--- index.ts
+++
@@ -1,6 +0,0 @@
-export { addContext, compact, Context } from './profile/context';
-export { toGraphObjects, toJsonLd } from './profile/jsonLd';
-export * from './profile/namespaces';
-export { QuadStore } from './store/quadStore';
-export { createOrGetStore } from './store/quadStoreFactory';
-
